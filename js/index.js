2405c073a40f09f59c990f11628d87413f8aaaaf
chenanjie <chenanjievip@163.com>
2016-10-07 15:52:09 +0800

    解决跨域问题

--- js/index.js
+++ js/index.js
@@ -3,6 +3,7 @@ app.controller('mainCtr', function($scope,$http,$location){
 	$http({
 		method:'get',
 		url:'http://api.tianapi.com/social/?key=d6142f13fc0a3c38605e660347e2d624&num=20',
+		dataType:'jsonp'
 	}).success(function(data){
 		if(data.code == 200){
 			$scope.newslist = data.newslist;
